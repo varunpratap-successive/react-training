54b951352c7a56a34e56fb695f0abd472f719b74
varun.singh1@successive.tech <varun.pratap@varunpratapr-350.successivetech.com>
2024-04-05 12:53:51 +0530

    completed assignment 3

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Assignment1 from "./Assignment1/Assignment1";
 import Assignment2 from "./Assignment2/Assignment2";
+import Assignment3 from "./Assignment3/Assignment3";
+
 function App() {
   return (
     <>
@@ -12,6 +14,10 @@ function App() {
         <div id="ass2">
           <h1>Assignment2</h1>
           <Assignment2 />
+        </div>
+        <div id="ass3">
+          <h1>Assignment3</h1>
+          <Assignment3 />
         </div>
       </div>
     </>
